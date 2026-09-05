de1eb16b9e8dfe492284d16b83f9abe93fa37d51
Suyash Gupta <suyashguptapc@gmail.com>
2026-09-06 00:10:57 +0530

    some changes

--- js/translation.js
+++ js/translation.js
@@ -72,6 +72,7 @@ window.googleTranslateElementInit = function() {
               </p>
             </div>
             <button class="lang-modal-close-btn notranslate" id="lang-modal-close" translate="no" aria-label="Close language selector">&times;</button>
+            <button class="lang-modal-close-btn notranslate" id="lang-modal-close" translate="no" aria-label="Close language selector" title="Close language selector"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
           </div>
 
           <div class="lang-modal-search notranslate" translate="no">
